483bc5060ba644a8fe7bc7159be92f0326defd3e
jmontleo <jmontleo@minishift-client.ose.int>
2017-05-22 17:07:42 -0400

    well.. it works

--- mediawiki123:latest/entrypoint.sh
+++ mediawiki123:latest/entrypoint.sh
@@ -1,41 +1,41 @@
 #!/bin/bash
 
-set -e
+#set -e
 
 : ${MEDIAWIKI_SITE_NAME:=MediaWiki}
 : ${MEDIAWIKI_SITE_LANG:=en}
 : ${MEDIAWIKI_ADMIN_USER:=admin}
 : ${MEDIAWIKI_ADMIN_PASS:=rosebud}
 : ${MEDIAWIKI_DB_TYPE:=postgres}
-: ${MEDIAWIKI_DB_SCHEMA:=mediawiki}
+: ${MEDIAWIKI_DB_SCHEMA:=wiki}
 # : ${MEDIAWIKI_ENABLE_SSL:=false}
 # : ${MEDIAWIKI_UPDATE:=false}
 
-if [ -z "$MEDIAWIKI_DB_HOST" -a -z "$MEDIAWIKI_DB_PORT" ]; then
-    echo >&2 'error: missing MEDIAWIKI_DB_HOST|MEDIAWIKI_DB_PORT environment variable'
-    exit 1
-fi
-
-
-: ${MEDIAWIKI_DB_USER:=postgres}
-if [ "$MEDIAWIKI_DB_USER" = 'root' ]; then
-    : ${MEDIAWIKI_DB_PASSWORD:=$ROOT_PASSWORD}
-fi
-: ${MEDIAWIKI_DB_NAME:=mediawiki}
-
-if [ -z "$MEDIAWIKI_DB_PASSWORD" ]; then
-    echo >&2 'error: missing required MEDIAWIKI_DB_PASSWORD environment variable'
-    echo >&2 '  Did you forget to -e MEDIAWIKI_DB_PASSWORD=... ?'
-    exit 1
-fi
-
-set -x
-export PGPASSWORD=$MEDIAWIKI_DB_PASSWORD
-psql -U $MEDIAWIKI_DB_USER -h $MEDIAWIKI_DB_HOST -p $MEDIAWIKI_DB_PORT -tc "SELECT 1 FROM pg_database WHERE datname = '$MEDIAWIKI_DB_NAME'" | grep -q 1 || (echo "$MEDIAWIKI_DB_NAME does not exist" && exit)
-
-unset PGPASSWORD
-
-
+#if [ -z "$MEDIAWIKI_DB_HOST" -a -z "$MEDIAWIKI_DB_PORT" ]; then
+#    echo >&2 'error: missing MEDIAWIKI_DB_HOST|MEDIAWIKI_DB_PORT environment variable'
+#    exit 1
+#fi
+#
+#
+#: ${MEDIAWIKI_DB_USER:=postgres}
+#if [ "$MEDIAWIKI_DB_USER" = 'root' ]; then
+#    : ${MEDIAWIKI_DB_PASSWORD:=$ROOT_PASSWORD}
+#fi
+#: ${MEDIAWIKI_DB_NAME:=mediawiki}
+#
+#if [ -z "$MEDIAWIKI_DB_PASSWORD" ]; then
+#    echo >&2 'error: missing required MEDIAWIKI_DB_PASSWORD environment variable'
+#    echo >&2 '  Did you forget to -e MEDIAWIKI_DB_PASSWORD=... ?'
+#    exit 1
+#fi
+#
+#set -x
+#export PGPASSWORD=$MEDIAWIKI_DB_PASSWORD
+#psql -U $MEDIAWIKI_DB_USER -h $MEDIAWIKI_DB_HOST -p $MEDIAWIKI_DB_PORT -tc "SELECT 1 FROM pg_database WHERE datname = '$MEDIAWIKI_DB_NAME'" | grep -q 1 || (echo "$MEDIAWIKI_DB_NAME does not exist" && exit)
+#
+#unset PGPASSWORD
+#
+#
 if [ ! -e "/persistent/LocalSettings.php" ]; then
   # If the container is restarted this will fail because the tables are already created
   # but there won't be a LocalSettings.php
@@ -50,8 +50,8 @@ if [ ! -e "/persistent/LocalSettings.php" ]; then
     --dbpass "$MEDIAWIKI_DB_PASSWORD" \
     --installdbuser "$MEDIAWIKI_DB_USER" \
     --installdbpass "$MEDIAWIKI_DB_PASSWORD" \
-    --server "$MEDIAWIKI_SITE_SERVER" \
     --scriptpath "" \
+    --server "http://${MEDIAWIKI_SITE_SERVER}" \
     --lang "$MEDIAWIKI_SITE_LANG" \
     --pass "$MEDIAWIKI_ADMIN_PASS" \
     "$MEDIAWIKI_SITE_NAME" \
@@ -60,8 +60,8 @@ if [ ! -e "/persistent/LocalSettings.php" ]; then
 else
   cp /persistent/LocalSettings.php /var/www/mediawiki123/LocalSettings.php
 fi
-
-export MEDIAWIKI_SITE_NAME MEDIAWIKI_DB_HOST MEDIAWIKI_DB_USER MEDIAWIKI_DB_PASSWORD MEDIAWIKI_DB_NAME
+#
+#export MEDIAWIKI_SITE_NAME MEDIAWIKI_DB_HOST MEDIAWIKI_DB_USER MEDIAWIKI_DB_PASSWORD MEDIAWIKI_DB_NAME
 
 /sbin/httpd -DFOREGROUND
-bash
+#bash
