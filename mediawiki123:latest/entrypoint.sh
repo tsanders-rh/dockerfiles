5991cf7187c8dd26868875073965f369c067df23
Fabian von Feilitzsch <fabian@fabianism.us>
2017-05-26 15:28:07 -0400

    add image directory and update settings

--- mediawiki123:latest/entrypoint.sh
+++ mediawiki123:latest/entrypoint.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -e
-
 USER_ID=$(id -u)
 if [ ${USER_UID} != ${USER_ID} ]; then
   sed "s@${USER_NAME}:x:\${USER_ID}:@${USER_NAME}:x:${USER_ID}:@g" ${BASE_DIR}/etc/passwd.template > /etc/passwd
@@ -36,6 +34,8 @@ psql -U $POSTGRESQL_USER -h $POSTGRESQL_HOST -p $POSTGRESQL_PORT -tc "SELECT 1 F
 unset PGPASSWORD
 
 
+LOCAL_SETTINGS=${BASE_DIR}/httpd/mediawiki123/LocalSettings.php
+IMAGE_DIR=${BASE_DIR}/httpd/mediawiki123/images
 if [ -d "$MEDIAWIKI_SHARED" ]; then
   if [ ! -e "$MEDIAWIKI_SHARED/LocalSettings.php" ] && [ ! -z "${POSTGRESQL_HOST}" ]; then
     # If the container is restarted this will fail because the tables are already created
@@ -57,17 +57,19 @@ if [ -d "$MEDIAWIKI_SHARED" ]; then
       --pass "$MEDIAWIKI_ADMIN_PASS" \
       "$MEDIAWIKI_ADMIN_USER" \
       "$MEDIAWIKI_SITE_NAME"
-    echo "session_save_path(\"${BASE_DIR}/tmp\");" >> ${BASE_DIR}/httpd/mediawiki123/LocalSettings.php
-    # echo "\$wgDebugLogFile = \"${BASE_DIR}/tmp/debug.log\";" >> ${BASE_DIR}/httpd/mediawiki123/LocalSettings.php
-    mv ${BASE_DIR}/httpd/mediawiki123/LocalSettings.php $MEDIAWIKI_SHARED/LocalSettings.php
-    ln -s $MEDIAWIKI_SHARED/LocalSettings.php ${BASE_DIR}/httpd/mediawiki123/LocalSettings.php
+    echo "session_save_path(\"${BASE_DIR}/tmp\");" >> $LOCAL_SETTINGS
+    sed -i -e "s/\$wgEnableUploads = false;/\$wgEnableUploads = true;/" $LOCAL_SETTINGS
+    sed -i -e "s/#\$wgHashedUploadDirectory = false;/\$wgHashedUploadDirectory = true;/" $LOCAL_SETTINGS
+    grep -q -F "\$wgUploadDirectory" "$LOCAL_SETTINGS" || (echo "\$wgUploadDirectory = \"$IMAGE_DIR\";" >> $LOCAL_SETTINGS)
+
+    mv $LOCAL_SETTINGS $MEDIAWIKI_SHARED/LocalSettings.php
+    ln -s $MEDIAWIKI_SHARED/LocalSettings.php $LOCAL_SETTINGS
   elif [ -e "$MEDIAWIKI_SHARED/LocalSettings.php" ]; then
-    ln -s $MEDIAWIKI_SHARED/LocalSettings.php ${BASE_DIR}/httpd/mediawiki123/LocalSettings.php
+    ln -s $MEDIAWIKI_SHARED/LocalSettings.php $LOCAL_SETTINGS
   fi
 
   # If the images directory only contains a README, then link it to
   # $MEDIAWIKI_SHARED/images, creating the shared directory if necessary
-  IMAGE_DIR=${BASE_DIR}/httpd/mediawiki123/images
   if [ "$(ls $IMAGE_DIR)" = "README" -a ! -L $IMAGE_DIR ]; then
     rm -rf $IMAGE_DIR
     mkdir -p "$MEDIAWIKI_SHARED/images"
@@ -75,9 +77,9 @@ if [ -d "$MEDIAWIKI_SHARED" ]; then
   fi
 fi
 
-if [ -e "${BASE_DIR}/httpd/wikimedia123/LocalSettings.php" -a $MEDIAWIKI_UPDATE = true ]; then
+if [ -e "$LOCAL_SETTINGS" -a "$MEDIAWIKI_UPDATE" = true ]; then
   echo >&2 'info: Running maintenance/update.php';
-  php /usr/share/mediawiki123/maintenance/update.php --quick --conf ${BASE_DIR}/httpd/wikimedia123/LocalSettings.php
+  php /usr/share/mediawiki123/maintenance/update.php --quick --conf $LOCAL_SETTINGS
 fi
 
 /sbin/httpd -DFOREGROUND -f ${BASE_DIR}/httpd/conf/httpd.conf
